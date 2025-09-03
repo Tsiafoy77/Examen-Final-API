387fb673a4e598043d81e0698c931cffdb78cff6
Tsiafoy <hei.tsiafouy@gmail.com>
2025-09-03 09:38:39 +0200

    Question numéro 1

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastAPI import FastAPI
+from fastapi import FastAPI
 
 app = FastAPI()
+phones_db=[]
+Characteristic={
+    ram_memory: int,
+    rom_memory: int
+}
+class phones(BaseModel):
+    id: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+@app.get("/health")
+def health():
+    return "OK"
+
+@app.post("/phones",status_code=200)
+async def create_phones(phones:List[phones]):
+    phones_db.extend(posts)
+    return phones_db
